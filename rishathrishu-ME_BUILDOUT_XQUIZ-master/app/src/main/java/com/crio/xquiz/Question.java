9b504643d6d6435ebaa4bc1853155a613d89e218
Rishathrishu <86212966+Rishathrishu@users.noreply.github.com>
2025-02-04 21:17:53 +0530

    Add files via upload

---
+++ rishathrishu-ME_BUILDOUT_XQUIZ-master/app/src/main/java/com/crio/xquiz/Question.java
@@ -0,0 +1,88 @@
+package com.crio.xquiz;
+import java.util.*;
+
+public class Question{
+        private String questionText;
+        private String answerText;
+        private List<String> choices;
+
+    //TODO: Define the variable to store the question text
+    //TODO: Define the variable to store the answer text
+    //TODO: Define the variable to store the list of text choices
+
+
+    // TODO: Create the Question class constructor 
+    public Question(String questionText, List<String> choices, String answerText){
+        if (questionText == null || questionText.isEmpty()){  // false
+                System.out.println("Question text cannot be null or empty!");
+        } 
+        else if (choices == null || choices.isEmpty()){ // false
+                System.out.println("Choices cannot be null or empty!");
+        }
+         else if (answerText == null || answerText.isEmpty()){ //false
+                System.out.println("Answer cannot be null or empty!");
+        }
+        else if(!choices.contains(answerText)){ //false
+                System.out.println("Answer is not present among the choices!");
+        }
+        else{
+                this.questionText = questionText;
+                this.answerText = answerText;
+                this.choices = choices;
+        }
+
+
+    }
+    // Include the following validations
+
+        // Validate: If QuestionText is  null or empty
+                // Print "Question text cannot be null or empty!"
+        // Else If List of Choices is null or EmptyList,
+                //  print “Choices cannot be null or empty!”
+        // Else If answer is  null or Empty
+                // print “Answer cannot be null or empty!”
+        // Else If answer is not one of the choices in the List of choices by iterating over the list
+                // print “Answer is not present among the choices!”
+        // Else
+                // initialise all the defined variables using `this` keyword
+            
+                
+    // Note: The print statements should match exactly with the one specified above, since we have test cases designed on this
+    // If the print statements do not match then the tests might fail, so be cautious
+
+    //TODO: Create the getter method : public String getAnswer(), which returns the answer
+    public String getAnswer(){
+        return answerText;
+    }
+
+    //TODO: Create the getter method : public String getQuestionText(), which returns the questionText
+    public String getQuestionText(){
+        return questionText;
+    }
+
+    //TODO: Create the getter method : public List<String> getChoices(), which returns the choices
+    public List<String> getChoices(){
+        return choices;
+    }
+
+    //TODO: Create the method checkAnswer() with signature: public boolean checkAnswer(String answer)
+        // Validate: If answer provided in the input matches the existing answer for the given question, then return True
+        // Else:  return False.
+        public boolean checkAnswer(String answer){
+                if(answer.equals(answerText)){
+                     return true;
+                } else {
+                    return false;
+                }
+        }
+
+
+public void display(){
+    System.out.println(getQuestionText());
+    for(int i = 0; i < choices.size(); i++){
+        int choiceNumber = i + 1;
+        System.out.println(choiceNumber + ":" + choices.get(i));
+    }
+}
+
+}
